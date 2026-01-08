cac41c8fd4815cbc0df6a8ad3354e3a9b7585dab
Anurag Thakur <161373638+NinjaKnight05@users.noreply.github.com>
2026-01-08 13:41:01 +0530

    Update corns.py

--- corn/corns.py
+++ corn/corns.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as pc
-import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as components
 import base64 
