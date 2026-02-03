9214cfe8d31bbdfebfe95ac0150fb0af68747cde
NinjaKnight05 <161373638+NinjaKnight05@users.noreply.github.com>
2026-02-03 15:05:45 +0530

    d

--- car_pred/car_pred.py
+++ car_pred/car_pred.py
@@ -5,6 +5,7 @@ from streamlit_option_menu import option_menu
 import base64
 import plotly.express as px
 import pickle
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title='CarPredictor',page_icon='🏎️')
 
@@ -12,13 +13,48 @@ menu = option_menu(menu_title='',options=['Home','Prediction','Analysis'],icons=
 
 model = joblib.load('car_model.joblib')
 fuelmod=pickle.load(open('fuel.pkl','rb'))
-sellmod = pickle.load(open('sell.pkl','rb'))
+ownmod = pickle.load(open('own.pkl','rb'))
 transmod = pickle.load(open('trans.pkl','rb'))
 
 
 
 if menu=='Home':
-    st.write('Home Page')
+   st.title('𝗩𝗘𝗛𝗜𝗖𝗟𝗘 𝗣𝗥𝗜𝗖𝗘 𝗘𝗦𝗧𝗜𝗠𝗔𝗧𝗢𝗥')
+   st.image('https://images.pexels.com/photos/70912/pexels-photo-70912.jpeg?cs=srgb&dl=pexels-tdcat-70912.jpg&fm=jpg')
+
+   st.subheader(
+    "Get an instant, machine-learning–based estimate of your car’s resale value."
+)
+
+
+   st.markdown("""
+## 📘 About
+
+This **Car Price Prediction** application uses **Machine Learning** to estimate the resale value of a car based on key features such as car age, fuel type, transmission, kilometers driven, seller type, and owner count.
+
+The model is trained on historical car sales data and is designed to provide quick, data-driven price estimates to help users make informed decisions.
+               
+
+""")
+   st.markdown("""
+### 📊 Factors affecting price
+- Newer cars tend to have higher resale value  
+- Lower mileage increases price  
+- Fuel type and transmission impact demand  
+- Fewer owners usually increase value
+""")
+   
+   st.markdown("""
+### 👥 Who can use this?
+- Individuals planning to sell a used car  
+- Buyers evaluating a fair market price  
+- Learners exploring real-world ML applications
+""")
+   
+   st.subheader("""⚠️ Important Note
+Predictions are reliable only for inputs that fall within the range of the training data. Extreme or unrealistic values may produce inaccurate results.""")
+
+
 
 elif menu=='Prediction':
     # st.subheader('ō͡≡o ✇⛟ ⚡︎⏲')
@@ -29,15 +65,15 @@ elif menu=='Prediction':
     fuel= st.selectbox("𝗙𝗨𝗘𝗟 ⛽",fuelmod.classes_)
     fuell=fuelmod.transform([fuel])[0]
 
-    sell= st.selectbox("𝗦𝗘𝗟𝗟𝗘𝗥 🧑‍💼",sellmod.classes_)
-    selll=sellmod.transform([sell])[0]
+    own= st.selectbox("𝗢𝗪𝗡𝗘𝗥 𝗖𝗢𝗨𝗡𝗧 🧑‍💼",ownmod.classes_)
+    # ownn=own.transform([own])[0]
 
     trans = st.selectbox("𝗧𝗥𝗔𝗡𝗦𝗠𝗜𝗦𝗦𝗜𝗢𝗡 ⚙️",transmod.classes_)
     transs=transmod.transform([trans])[0]
 
 
 
-    st.session_state.pred=[i1,i2,i3,fuell,selll,transs]
+    st.session_state.pred=[i1,i2,i3,fuell,own,transs]
 
 
     btn1= st.button('Submit')
@@ -46,16 +82,5 @@ elif menu=='Prediction':
      st.write(res)
      st.success('Done')
 
-
-
-
-
-
-
-
-
-
-
-
 else:
-    st.write('Analysis')
+    st.title('Analysis')
